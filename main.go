8d67884e9a865e49e96c90c35b09c75df1e74111
Weslei Ferreira <weslei.ferreira@twism.com>
2025-06-01 13:59:39 -0300

    feat: Add Custom Log

--- main.go
+++ main.go
@@ -1,8 +1,27 @@
 package main
 
-import "github.com/WesleiDev/api-go-estudo/router"
+import (
+	"github.com/WesleiDev/api-go-estudo/config"
+	"github.com/WesleiDev/api-go-estudo/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main(){
+
+	logger = config.GetLogger("main")
+	logger.Info("Loading server...")
+
+	//Initialize Configs
+	err := config.Init()
+	
+	if(err != nil){
+		logger.ErrorF("config initialization error: %v", err)
+		return
+	}
+
 	//Initialize Router
 	router.Initialize() 
 }
